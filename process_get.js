9917e4a360204513d400040035c1675dec3fac3b
Jack.study <njzhch@163.com>
2021-03-07 16:55:34 -0500

    front req ok

--- process_get.js
+++ process_get.js
@@ -12,7 +12,6 @@ const router_get = express.Router();
 
 var mimeType = public_data.mimeType;
 
-// 处理表单提交，对应请求头application/x-www-form-urlencoded
 router_get.use(
   bodyParser.urlencoded({
     extended: false // 为true时将使用qs库处理数据，通常不需要
@@ -21,14 +20,16 @@ router_get.use(
 
 var root = path.resolve(process.argv[2] || "../skynocean/dist");
 
-router_get.get("/picture", (req, res, next) => {
-  var filepath = path.join(root, "cat_1.png");
+router_get.get("/picture/:name", (req, res, next) => {
+  var file_name = req.url.slice(9);
+
+  var filepath = path.join(root, file_name);
 
   const ext = path.parse(filepath).ext;
 
   fs.readFile(filepath, (err, data) => {
     if (!err) {
-      console.log(filepath + " read success");
+      /* console.log(filepath + " read success");*/
 
       /* res.setHeader("Content-type", mimeType[ext] || "text/plain");*/
       res.setHeader(
@@ -44,8 +45,26 @@ router_get.get("/picture", (req, res, next) => {
   });
 });
 
-router_get.get("/json", (req, res, next) => {
-  res.end(JSON.stringify(public_data.product_catalog));
+router_get.get("/json/:name", (req, res, next) => {
+  var file_name = req.url.slice(6);
+
+  var json_obj;
+
+  switch (file_name) {
+    case "product":
+      json_obj = public_data.product;
+      break;
+
+    case "product_catalog":
+      json_obj = public_data.product_catalog;
+      break;
+
+    default:
+      json_obj = { id: 9999, msg: "not find" };
+      break;
+  }
+
+  res.end(JSON.stringify(json_obj));
 });
 
 module.exports = router_get;
